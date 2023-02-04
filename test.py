6efd4c62d2fbdd98a4527b5dd7a7f106bbb8e20c
haissaoui <42788676+haissaoui@users.noreply.github.com>
2023-02-04 23:50:46 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,6 @@
-
+# This function convert the interest rate into discount rate
+def interest_to_discount_converter(i):
+    d = 1 - 1 / (1+i)
+    return d
+i = 0.06
+print(interest_to_discount_converter(i))
