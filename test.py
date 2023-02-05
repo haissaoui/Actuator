d026d976c185be4fae4d1aeb1ff7a874037b788f
haissaoui <42788676+haissaoui@users.noreply.github.com>
2023-02-05 16:58:42 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,34 @@
-# This function convert the interest rate into discount rate
+# This function determine the effective rate of interest
+def effective_rate_of_interest(a_t, a_t_m_1):
+    i_t = (a_t - a_t_m_1) / a_t_m_1
+    return i_t
+
+
+# This function determine the effective rate of discount
+def effective_rate_of_discount(a_t, a_t_m_1):
+    d_t = (a_t - a_t_m_1) / a_t
+    return d_t
+
+
+# This function calculate the accumulated value
+def accumulated_value(a_0, i, t):
+    a_v = a_0 * (1 + i) ** t
+    return a_v
+
+
+# This function convert the rate of interest to rate of discount
 def interest_to_discount_converter(i):
     d = 1 - 1 / (1+i)
     return d
-i = 0.06
-print(interest_to_discount_converter(i))
+
+
+# This function convert the rate of discount to rate of interest
+def discount_to_interest_converter(d):
+    i = 1/(1 - d) - 1
+    return i
+
+
+# This function convert effective annual rate of interest to nominal periodic rate
+def nominal_to_effective_interest_rate(i, m):
+    i_m = m * ((1 + i) ** (1 / m) - 1)
+    return i_m
