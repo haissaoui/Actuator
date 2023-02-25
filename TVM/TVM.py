cc7836a0cb02ba688b1d036a256b885e438cd70b
haissaoui <42788676+haissaoui@users.noreply.github.com>
2023-02-25 19:54:55 +0100

    #3

--- TVM/TVM.py
+++ TVM/TVM.py
@@ -2,3 +2,8 @@
 def tvm_unknown_fv(n, iy, pv, pmt):
     fv = pv * (1 + iy) ** n + pmt * ((1 + iy) ** n - 1) / iy
     return fv
+
+
+def tvm_unknown_pv(n, iy, pmt, fv):
+    pv = fv * (1 / (1 + i)) ** n + pmt * ((1 - (1 / (1 + i)) ** n) / iy)
+    return pv
