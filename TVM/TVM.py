e854b187fa7426ce77bdff2c63b78581560538c5
haissaoui <42788676+haissaoui@users.noreply.github.com>
2023-02-26 23:02:10 +0100

    Add new formula and correct the old ones (#3)

--- TVM/TVM.py
+++ TVM/TVM.py
@@ -1,9 +1,16 @@
 # TVM function where FV is unknown:
 def tvm_unknown_fv(n, iy, pv, pmt):
-    fv = pv * (1 + iy) ** n + pmt * ((1 + iy) ** n - 1) / iy
+    fv = -(pv * (1 + iy) ** n + pmt * ((1 + iy) ** n - 1) / iy)
     return fv
 
 
+# TVM function where PV is unknown:
 def tvm_unknown_pv(n, iy, pmt, fv):
-    pv = fv * (1 / (1 + i)) ** n + pmt * ((1 - (1 / (1 + i)) ** n) / iy)
+    pv = -(fv * (1 / (1 + iy)) ** n + pmt * ((1 - (1 / (1 + iy)) ** n) / iy))
     return pv
+
+
+# TVM function where PMT is unknown:
+def tvm_unknown_pmt(n, iy, pv, fv):
+    pmt = (-fv - pv * (1 + iy) ** n) / (((1 + iy) ** n - 1) / iy)
+    return pmt
