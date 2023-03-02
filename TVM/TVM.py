620651642c5035702014567a0eb690fc2ec5be9e
haissaoui <42788676+haissaoui@users.noreply.github.com>
2023-03-02 21:55:56 +0100

    Add new formula (#3)

--- TVM/TVM.py
+++ TVM/TVM.py
@@ -1,3 +1,7 @@
+# Importing libraries
+import numpy as np
+
+
 # TVM function where FV is unknown:
 def tvm_unknown_fv(n, iy, pv, pmt):
     fv = -(pv * (1 + iy) ** n + pmt * ((1 + iy) ** n - 1) / iy)
@@ -14,3 +18,9 @@ def tvm_unknown_pv(n, iy, pmt, fv):
 def tvm_unknown_pmt(n, iy, pv, fv):
     pmt = (-fv - pv * (1 + iy) ** n) / (((1 + iy) ** n - 1) / iy)
     return pmt
+
+
+# TVM function where n is unknown:
+def tvm_unknown_n(iy, pv, pmt, fv):
+    n = np.log((fv + pmt / iy) / (pv + pmt / iy)) / np.log(1 + iy)
+    return n
